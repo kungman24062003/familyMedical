5585d2798fb7d09d8ecb1c17b573a5ae91ae8d61
kungman24062003 <lenhkungman4344@gmail.com>
2025-10-28 23:33:32 +0700

    Initial commit - Spring Boot project

---
+++ src/test/java/com/example/familyMedical/FamilyMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familyMedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
